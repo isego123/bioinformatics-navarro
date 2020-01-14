b24f29fb2e826d2d4901b240f47214b3aa623a15
DesikaKolaric <desika.kolaric@gmail.com>
2020-01-14 17:12:35 +0100

    Propagate added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 #include "BigraphToDigraph.h"
 using namespace std;
 
+vector<int> in[MAXN];
+vector<int> out[MAXN];
+
+void propagate(int u, int v, AlignmentGraph& graph, vector<int> &Cold, vector<int> &Cnew) {
+    if(Cnew[v] > 1 + Cnew[u]) {
+        Cnew[v] = 1 + Cnew[u];
+        for(int i = 0; i < out[v].size(); i++) {
+            propagate(v, out[v][i], graph, Cold, Cnew);
+        }
+    }
+}
+
 int main(void) {
     auto fastqs = loadFastqFromFile("./input/ref10000_simulatedreads.fastq");
     auto alignmentGraph = DirectedGraph::StreamGFAGraphFromFile("./input/ref10000_linear.gfa");
